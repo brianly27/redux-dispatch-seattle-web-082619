b28f7d99cc24e161f1810d215f9aae0772d8b067
Brian Ly <brian.kyang.ly@gmail.com>
2019-11-19 11:54:49 -0800

    finished lab

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,26 @@
-function changeState(state, action){
+function changeState(state, action) {
   switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
+    case "INCREASE_COUNT":
+      return { count: state.count + 1 };
     default:
       return state;
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+let state = { count: 0 };
+let action = { type: "INCREASE_COUNT" };
 
-changeState(state, action)
+changeState(state, action);
+
+const dispatch = action => {
+  state = changeState(state, action);
+  render();
+};
+
+const render = () => {
+  document.body.textContent = state.count;
+};
+
+dispatch(action);
+dispatch(action);
+dispatch(action);
